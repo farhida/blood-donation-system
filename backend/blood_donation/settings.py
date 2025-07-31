e3034d4ff451dda9dec141b90ec606ff007dd323
farhida <toushifarhidaislam@gmail.com>
2025-07-31 12:07:46 +0600

    Setup PostgreSQL

--- backend/blood_donation/settings.py
+++ backend/blood_donation/settings.py
@@ -74,8 +74,12 @@ WSGI_APPLICATION = 'blood_donation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'blood_donation_db',
+        'USER': 'postgres',
+        'PASSWORD': 'farhiz23',  # Replace with your PostgreSQL password
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
